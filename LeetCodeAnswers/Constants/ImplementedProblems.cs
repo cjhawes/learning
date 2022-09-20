018d2e7ee5b0c8108808585d595781ae1b362f60
Connor <cjhawes.cs@gmail.com>
2022-09-20 22:54:41 +0100

    Complete setup

--- LeetCodeAnswers/Constants/ImplementedProblems.cs
+++ LeetCodeAnswers/Constants/ImplementedProblems.cs
@@ -1,5 +1,13 @@
-﻿using LeetCodeAnswers.Problems.LeetOne;
+﻿using LeetCodeAnswers.Problems.LeetFifteen;
+using LeetCodeAnswers.Problems.LeetFiftySix;
+using LeetCodeAnswers.Problems.LeetOne;
+using LeetCodeAnswers.Problems.LeetOneHundredAndTwo;
+using LeetCodeAnswers.Problems.LeetSeventyTwo;
+using LeetCodeAnswers.Problems.LeetThirtyFour;
+using LeetCodeAnswers.Problems.LeetTwentyOne;
 using LeetCodeAnswers.Problems.LeetTwo;
+using LeetCodeAnswers.Problems.LeetTwoHundred;
+using LeetCodeAnswers.Problems.LeetTwoHundredAndSix;
 
 namespace LeetCodeAnswers.Constants;
 
@@ -9,5 +17,13 @@ public static class ImplementedProblems
 	{
 		{ 1, new Action(LeetOne.Run) },
 		{ 2, new Action(LeetTwo.Run) },
+		{ 15, new Action(LeetFifteen.Run) },
+		{ 21, new Action(LeetTwentyOne.Run) },
+		{ 34, new Action(LeetThirtyFour.Run) },
+		{ 56, new Action(LeetFiftySix.Run) },
+		{ 72, new Action(LeetSeventyTwo.Run) },
+		{ 102, new Action(LeetOneHundredAndTwo.Run) },
+		{ 200, new Action(LeetTwoHundred.Run) },
+		{ 206, new Action(LeetTwoHundredAndSix.Run) },
 	};
 }
---
+++ LeetCodeAnswers/Problems/LeetTwoHundredAndSix/LeetTwoHundredAndSix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAnswers.Problems.LeetTwoHundredAndSix;
+
+public class LeetTwoHundredAndSix
+{
+	public static void Run()
+	{
+		ReverseLinkedList();
+	}
+
+	private static void ReverseLinkedList()
+	{
+		return;
+	}
+}
