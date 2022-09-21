e2c37bbe1055b0ef2a783d6ed5c334a8c5ffe6f9
Connor <cjhawes.cs@gmail.com>
2022-09-21 23:02:06 +0100

    Implement problem 15

--- LeetCodeAnswers/Problems/LeetFifteen/LeetFifteen.cs
+++ LeetCodeAnswers/Problems/LeetFifteen/LeetFifteen.cs
@@ -6,10 +6,57 @@ public class LeetFifteen
 	{
 		var nums = new int[] { -1, 0, 1, 2, -1, -4 };
 		var ans = ThreeSum(nums);
+
+        foreach (var a in ans)
+		{
+            Console.WriteLine($"[{a[0]}, {a[1]}, {a[2]}]");
+		}
 	}
 
 	private static IList<IList<int>> ThreeSum(int[] nums)
 	{
-		return null!;
-	}
+        IList<IList<int>> result = new List<IList<int>>();
+
+        if (nums.Length <= 2) return result;
+
+        Array.Sort(nums);
+
+        int start = 0, left, right;
+        int target;
+
+        while (start < nums.Length - 2)
+        {
+            target = nums[start] * -1;
+            left = start + 1;
+            right = nums.Length - 1;
+
+            while (left < right)
+            {   
+                if (nums[left] + nums[right] > target)
+                {
+                    --right;
+                }
+                else if (nums[left] + nums[right] < target)
+                {
+                    ++left;
+                }
+                else
+                {
+                    List<int> OneSolution = new List<int>() { nums[start], nums[left], nums[right] };
+                    result.Add(OneSolution);
+
+                    while (left < right && nums[left] == OneSolution[1])
+                        ++left;
+                    while (left < right && nums[right] == OneSolution[2])
+                        --right;
+                }
+
+            }
+
+            int currentStartNumber = nums[start];
+            while (start < nums.Length - 2 && nums[start] == currentStartNumber)
+                ++start;
+        }
+        return result;
+    }
 }
